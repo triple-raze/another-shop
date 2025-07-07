e6e2088f33b2cf48e044829447c2895f2c4a70e0
triple raze <weebucks2@gmail.com>
2025-07-07 08:43:20 +0300

    changed password char limit

--- server/models/user.py
+++ server/models/user.py
@@ -7,5 +7,5 @@ class UserModel(BaseModel):
     name: str = Field(max_length=50)
     gender: Literal['male', 'female']
     email: EmailStr = Field(max_length=150)
-    password: str = Field(max_length=30)
+    password: str = Field(max_length=50)        # not hashed
     registration_timestamp: date = date.today()
