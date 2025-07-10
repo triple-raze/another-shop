9b45f902b29171dda4c26984d553f1196cd11e3c
Ratimir838 <grudinskijratimir838@gmail.com>
2025-07-10 21:08:08 +0300

    Перезалив

--- client/js/main-page.js
+++
@@ -1,8 +0,0 @@
-fetch("/users", {
-method: "POST",
-headers: {"Content-Type": "application/json"},
-body: JSON.stringify(user: document.getElementById("tovary").value
-})
-.then(response => response.json())
-.then(data  => console.log(data))
-.catch(error => console.log(error))
