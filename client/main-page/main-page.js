8307cd4588a462c20a74efac56d66babc25ff3e8
Ratimir838 <grudinskijratimir838@gmail.com>
2025-07-14 10:29:46 +0300

    Add files via upload

--- client/main-page/main-page.js
+++ client/main-page/main-page.js
@@ -1,7 +1,7 @@
 document.getElementById('search-form').addEventListener('submit', function(event) {
   event.preventDefault();
   const searchTerm = document.getElementById('search-input').value.toLowerCase();
-  const elements = document.querySelectorAll('slot');
+  const elements = document.querySelectorAll('div');
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
     const text = element.textContent.toLowerCase();
